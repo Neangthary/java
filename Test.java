ff94eebdc57954afb3499b3ee8b4393a907264ac
Neangthary <neangthary210@gmail.com>
2023-05-02 23:56:05 +0700

    Don't be sad. fun

--- Test.java
+++ Test.java
@@ -1,7 +1,13 @@
 public class Test {
   public static void main(String[] args) {
-    System.out.println("Hello");
-    System.out.println("Java");
-    System.out.println("05" + "/" + 2023);
+    System.out.println("  __");
+    System.out.println(" |" + "__" + "|");
+    System.out.println("_______");
+    System.out.println("_______");
+    System.out.println("|     |");
+    System.out.println("|     |" + "    SAD EY   ");
+    System.out.println("   |   ");
+
+    
   }
 }
